a8b2fb693c19ba1d36d81018331e54f4581099f0
Alex <Alex@WIN-TSK25FK506L.lan>
2015-12-10 23:13:04 +0800

    2.0

--- nutz_base/src/test/java/nutz_base/Test.java
+++ nutz_base/src/test/java/nutz_base/Test.java
@@ -9,7 +9,7 @@ public class Test {
 	public static void main(String[] args) {
 		try {
 			System.out.println(DateUtils.parseDate("2015-12-10 22:58:00",
-					new String[] { "yyyy/MM/dd HH:mm:ss" }));
+					new String[] { "yyyy-MM-dd HH:mm:ss" }));
 		} catch (ParseException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
